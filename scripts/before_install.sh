0ca281950a379cf036a127eaf21e89949fcffde0
EC2 Default User <ec2-user@ip-172-31-7-64.ap-south-1.compute.internal>
2025-07-24 10:09:17 +0000

    edited

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-echo "Running BeforeInstall script..."
-
-# Stop existing containers (optional safety)
-docker-compose down || true
-
-# Remove unused images to save space
-docker image prune -af || true
+echo "Cleaning old deployment files..."
+rm -rf /home/ec2-user/CD-CP/*
